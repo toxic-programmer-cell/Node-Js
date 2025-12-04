f10dbc8d5fb9581f5ac9356aedb495c035337b75
rohit <rohitmahto062@gmail.com>
2025-12-04 13:41:14 +0530

    _

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const express = require("express");
 
 let app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
-});
-
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+  res.render("about");
 });
 
 // 404 page
 app.use((req, res) => {
-  res.sendFile("./views/error.html", { root: __dirname });
+  res.render("404");
 });
-
+debugger;
 //listen for request
-app.listen(4000, () => {
+app.listen(4000, (err) => {
+  if (err) {
+    console.log(err);
+  }
   console.log("server is running on port 4000");
 });
